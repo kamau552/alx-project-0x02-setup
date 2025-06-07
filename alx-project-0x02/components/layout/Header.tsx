71c1074017ff110009896a99838d59c76de7d90e
Njeri Kamau <faithkamau730@gmail.com>
2025-06-07 14:45:03 +0300

    adjusted home navigation

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="flex justify-between items-center bg-blue-500 text-white p-4">
       <h1 className="text-xl font-semibold">ALX Project</h1>
       <nav className="space-x-4">
-        <Link href="/">Home</Link>
+        <Link href="/home">Home</Link>
         <Link href="/about">About</Link>
       </nav>
     </header>
