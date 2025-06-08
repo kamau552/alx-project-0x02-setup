1f8f986ae894c1cd0640f62f11354b1478f07d0a
Njeri Kamau <faithkamau730@gmail.com>
2025-06-08 18:19:29 +0300

    header component included

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import Header from '../components/layout/Header';
+import React from 'react';
 import Button from '@/components/common/Button';
 
 export default function About() {
